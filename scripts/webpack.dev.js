31ebf597912305904090c45bfd1af85161936f50
rookie1989 <nichunlong@gmail.com>
2025-02-23 08:52:12 +0800

    优化代码

--- scripts/webpack.dev.js
+++ scripts/webpack.dev.js
@@ -3,9 +3,9 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./example/index.tsx", // 入口文件改为 example/index.tsx
+  entry: path.resolve(__dirname, "../example/index.tsx"), // 入口文件改为 example/index.tsx
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "../dist"),
     filename: "index.js",
     publicPath: "/",
     library: "TntReactComponent",
@@ -25,11 +25,35 @@ module.exports = {
         test: /\.less$/,
         use: ["style-loader", "css-loader", "less-loader"],
       },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: "url-loader",
+        options: {
+          limit: 10000,
+          name: "img/[name].[hash:7].[ext]",
+        },
+      },
+      {
+        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+        loader: "url-loader",
+        options: {
+          limit: 10000,
+          name: "media/[name].[hash:7].[ext]",
+        },
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: "url-loader",
+        options: {
+          limit: 10000,
+          name: "fonts/[name].[hash:7].[ext]",
+        },
+      },
     ],
   },
   devServer: {
     static: {
-      directory: path.join(__dirname, "example"),
+      directory: path.join(__dirname, "../example"),
     },
     hot: true,
     open: true,
--- scripts/webpack.prod.js
+++ scripts/webpack.prod.js
@@ -15,6 +15,20 @@ module.exports = {
   resolve: {
     extensions: [".ts", ".tsx", ".js", ".jsx"],
   },
+  externals: {
+    react: {
+      commonjs: "react",
+      commonjs2: "react",
+      amd: "react",
+      root: "React",
+    },
+    "react-dom": {
+      commonjs: "react-dom",
+      commonjs2: "react-dom",
+      amd: "react-dom",
+      root: "ReactDOM",
+    },
+  },
   module: {
     rules: [
       {
@@ -25,6 +39,30 @@ module.exports = {
       {
         test: /\.less$/,
         use: ["style-loader", "css-loader", "less-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: "url-loader",
+        options: {
+          limit: 10000,
+          name: "img/[name].[hash:7].[ext]",
+        },
+      },
+      {
+        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+        loader: "url-loader",
+        options: {
+          limit: 10000,
+          name: "media/[name].[hash:7].[ext]",
+        },
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: "url-loader",
+        options: {
+          limit: 10000,
+          name: "fonts/[name].[hash:7].[ext]",
+        },
       },
     ],
   },
